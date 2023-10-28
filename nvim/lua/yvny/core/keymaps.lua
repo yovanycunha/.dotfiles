e3959289af2950fa9ec38e70355d397a83bf805e
Yovany Cunha <yovany33@gmail.com>
2023-10-28 00:25:04 -0300

    nvim update keymaps

--- nvim/lua/yvny/core/keymaps.lua
+++ nvim/lua/yvny/core/keymaps.lua
@@ -2,6 +2,19 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+keymap.set("n", "<leader>nh", ":nohl<CR>") -- clear search highlights
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- previous tab
+
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle
 
